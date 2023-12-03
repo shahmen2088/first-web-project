f0291b3e367b1e95e64ff68b482146e2dc9f1337
Shokhrukh <shoxrux20888@mail.ru>
2023-12-03 17:41:30 +0300

    push

--- code.js
+++ code.js
@@ -1,20 +1,51 @@
-const encrypt = (text) => {
-    let result = '';
-    let prev = text[0];
-    for (let i = 1; i < text.length; i += 2) {
-      if (text.length % 2 === 0) {
-        result += text[i] + prev;
-        prev = text[i + 1];
-      } else {
-        result += text[i] + prev;
-        prev = text[i + 1];
-        
-      }
-      }
-      return result;
-    };
-  
-  // END
-  
-  export default encrypt;
-  
+$(function () {
+  //Fixed header
+
+  let header = $("#header"),
+    intro = $("#intro"),
+    introH = intro.innerHeight(),
+    scrollPosition = $(window).scrollTop(),
+    nav = $("#nav"),
+    navToggle = $("#nav__toggle");
+
+  console.log(scrollPosition);
+
+  $(window).on("scroll resize", function () {
+    introH = intro.innerHeight();
+    scrollPosition = $(this).scrollTop();
+    checkScroll(introH, scrollPosition);
+  });
+  header.addClass("header__fixed");
+
+  function checkScroll(introH, scrollPosition) {
+    if (scrollPosition > introH) {
+      header.addClass("header__fixed");
+    } else {
+      header.removeClass("header__fixed");
+    }
+  }
+
+  // Smooth scroll
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+    let blockId = $(this).data("scroll");
+    let blockOffset = $(blockId).offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+  });
+
+  //Nav Toggle
+  $(document).ready(function () {
+    $("menu-burger__header").click(function () {
+      $(this).toggleClass("open-menu");
+      $(nav).toggleClass("open-menu");
+    });
+  });
+});
+
+const person = (firstName, lastName) => ({ first: firstName, last: lastName });
+
